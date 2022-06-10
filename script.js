f662215904992d58af72eaf24a1696fd0b926fa8
helioramon <helio.alvareng@gmail.com>
2022-06-09 22:01:08 -0300

    completed data structure

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = "Helio"
+console.log (nome)
+let número = 8
+console.log (número)
+let ano = 2022
+console.log (ano)
+let resultado = 2022 - 8
+console.log (resultado)
+let ação = "estou mexendo no console.log"
+console.log(ação)
+let dia= "quartaFeira"
+let quartaFeira = "false"
+console.log (quartaFeira)
+
